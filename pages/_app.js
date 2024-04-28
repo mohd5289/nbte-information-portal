7f29c96c53a07dcad50d896258899bf59c50dd45
Osuolale <mohd@yahoo.com>
2024-04-28 17:08:24 +0100

    Added next/head in _my_app

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import "../styles/toastStyles.css";
 import "../styles/Backdrop.css";
 import "../styles/Modal.css";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <>
