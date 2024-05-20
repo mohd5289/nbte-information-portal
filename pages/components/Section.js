9a024ec5a036aec4c9dc3a8e5e21dbbce88c665f
Osuolale <mohd@yahoo.com>
2024-05-20 17:29:56 +0100

    Defaulted to picture

--- pages/components/Section.js
+++ pages/components/Section.js
@@ -183,91 +183,14 @@ const Section = () => {
           </div>
         </div>
         <div className="relative xl:absolute xl:flex w-80 h-80 xl:w-[800px] l:w-[400px] xl:h-[330px]  right-10 hidden sm:block md:w-64 md:h-64 m-auto md:hidden">
-          <motion.div
-            variants={carouselVariants}
-            initial="initial"
-            animate="animate"
-            exit="exit"
-            key={activeIndex}
-            className="carousel-item"
-          >
-            {activeIndex === 0 && (
-              <div className="text-lg font-bold italic">
-                <p className="text-lg">
-                  This is the Digital Edition of the Directory of accredited
-                  Programmes offered in all institutions in Nigeria under the
-                  regulation of NBTE. It contains the list of Programmes that
-                  are granted full accreditation and interim accreditation. It
-                  is published in exercise of the powers conferred on the Board
-                  by the Education (National Minimum Standards and Establishment
-                  of Institutions) CAP E3 LFN 2004. Accreditation has been
-                  granted to the following categories of institutions:
-                </p>
-                <ul className="list-none ml-6">
-                  <li>Polytechnics;</li>
-                  <li>Colleges of Agriculture;</li>
-                  <li>Colleges of Health Sciences;</li>
-                  <li>Specialized Institutions;</li>
-                  <li>Innovation Enterprise Institutions;</li>
-                  <li>Technical Colleges;</li>
-                  <li>Vocational Enterprise Institutions; and</li>
-                  <li>NSQ Training Providers/Centers/Institutions.</li>
-                </ul>
-              </div>
-            )}
-            {activeIndex === 1 && (
-              <div className="text-lg font-bold italic">
-                <p className="text-lg">
-                  Approved institutions offering the approved programmes can
-                  award the following certificates as the case may be and as
-                  appropriate to the graduates of the programmes:
-                </p>
-                <ul className="list-none ml-6">
-                  <li>Post Higher National Diploma (Post-HND);</li>
-                  <li>Higher National Diploma (HND);</li>
-                  <li>Higher National Innovation Diploma (HNID);</li>
-                  <li>National Diploma (ND);</li>
-                  <li>National Innovation Diploma (NID);</li>
-                  <li>Advanced National Business Certificate (ANBC);</li>
-                  <li>Advanced National Technical Certificate (ANTC);</li>
-                  <li>NSQ Training Providers/Centers/Institutions;</li>
-                  <li>National Technical Certificate (NTC);</li>
-                  <li>National Vocational Certificate (NVC);</li>
-                  <li>National Skills Qualifications (NSQs);</li>
-                </ul>
-              </div>
-            )}
-            {activeIndex === 2 && (
-              <div className="text-lg font-bold italic">
-                <p>
-                  The Accreditation granted to a programme is with effect from
-                  the latest date indicated in the expiration date column.
-                  Programmes granted Accreditation will be due for another visit
-                  after five calendar years. The purpose of publishing this
-                  Digital Directory is to inform academic institutions,
-                  Scholarship Boards, employers of labour, stakeholders in the
-                  technical education sector, parents and prospective students
-                  about the accreditation status of programmes that are offered
-                  by these institutions. Institutions whose programmes have been
-                  granted interim accreditation should take appropriate steps to
-                  ensure that they are visited when due and granted full
-                  accreditation status. I congratulate the institutions whose
-                  programmes have been granted full accreditation and implore
-                  them to strive to improve the quality of their programmes.
-                </p>
-                <div className="flex flex-col pt-10">
-                  <p className="text-left text-lg">
-                    {" "}
-                    Prof Idris Muhammed Bugaje
-                  </p>
-                  <p className="text-left text-lg font-bold">
-                    {" "}
-                    EXECUTIVE SECRETARY
-                  </p>
-                </div>
-              </div>
-            )}
-          </motion.div>
+          <Image
+            src={NBTE}
+            className="rounded-md"
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+            priority
+          />
         </div>
       </div>
     </main>
